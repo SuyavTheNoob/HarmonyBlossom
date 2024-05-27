7e1e355b9ce8baa5e721770a4c9c558920aaacbf
SuyavTheNoob <160936754+SuyavTheNoob@users.noreply.github.com>
2024-05-27 06:35:22 +0545

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 2323;
-app.get('/', (req, res) => res.send('Harmony is Alive!'));
+app.get('/', (req, res) => res.send('Blossom Harmony is Alive!'));
 
-app.listen(port, () => console.log(`Harmony is listening to http://localhost:${port}`));
+app.listen(port, () => console.log(`Blossom Harmony is listening to http://localhost:${port}`));
